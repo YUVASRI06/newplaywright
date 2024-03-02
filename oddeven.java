cd40b973be6d9418b491f48adc90f48bf2e31dc5
YUVASRI06 <yuvasriperumalsamy0609@gmail.com>
2024-03-02 15:45:09 +0530

    new pro

---
+++ oddeven.java
@@ -0,0 +1,9 @@
+
+public class EvenOdd {
+    public static void main(String[] args) {
+      for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+             System.out.println("Hello, " + name + "! Welcome.");
+      }
+    }
+}
